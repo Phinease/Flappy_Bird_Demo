d41a046e8681e13aabc05f75b692a582e8bb989b
Phinease <chenshuangrui@gmail.com>
2021-01-19 17:46:57 +0100

    Mac update

--- src/model/Etat.java
+++ src/model/Etat.java
@@ -4,32 +4,30 @@ import java.util.ArrayList;
 
 public class Etat {
     public static final int JUMP_HEIGHT = 60;
-    public static final int LARG_OVAL = 20;
-    public static final int HAUT_OVAL = 100;
     public static final int X_OVAL = 50;
 
-    public int Y_OVAL = 600;
+    public int Y_OVAL = 0;
     private ArrayList<Integer> history = new ArrayList<>();
 
     // chaque délai, l'oiseau tombe un peu
     public void drop() {
-        Y_OVAL += 2;
-        if (Y_OVAL > 700) Y_OVAL = 700;
+        Y_OVAL -= 2;
+        if (Y_OVAL < 0) Y_OVAL = 0;
     }
 
-    public int getHauteur() {
+    public int getHauteur(int max) {
+        if (Y_OVAL > max) Y_OVAL = max;
         return Y_OVAL;
     }
 
     // chaque click, l'oiseau saute mais jamais hors des limites
     public void jump() {
-        Y_OVAL -= JUMP_HEIGHT;
-        if (Y_OVAL < 0) Y_OVAL = 0;
+        Y_OVAL += JUMP_HEIGHT;
     }
 
     // Ajouter le point actuel dans le chemin
     public void addHistory() {
-        history.add(Y_OVAL + HAUT_OVAL / 2);
+        history.add(Y_OVAL);
         if (history.size() > 100) history = new ArrayList<>(history.subList(history.size() - 100, history.size()));
     }
 
--- src/view/Affichage.java
+++ src/view/Affichage.java
@@ -7,12 +7,14 @@ import java.awt.*;
 
 public class Affichage extends JPanel {
     private final Etat etat;
-    public static final int LARG = 800;
-    public static final int HAUT = 800;
+    public static final int LARGEUR_FENETRE = 800;
+    public static final int HAUTEUR_FENETRE = 800;
+    public static final int LARG_OVAL = 20;
+    public static final int HAUT_OVAL = 100;
 
     public Affichage(Etat e) {
         etat = e;
-        this.setPreferredSize(new Dimension(LARG, HAUT));
+        this.setPreferredSize(new Dimension(LARGEUR_FENETRE, HAUTEUR_FENETRE));
     }
 
     @Override
@@ -21,16 +23,18 @@ public class Affichage extends JPanel {
         super.paint(g);
 
         // dessiner l'oiseau
+        int hauteur = etat.getHauteur(HAUTEUR_FENETRE - HAUT_OVAL);
         g.setColor(Color.RED);
-        g.drawOval(Etat.X_OVAL, etat.getHauteur(), Etat.LARG_OVAL, Etat.HAUT_OVAL);
+        g.drawOval(Etat.X_OVAL, HAUTEUR_FENETRE - hauteur - HAUT_OVAL, LARG_OVAL, HAUT_OVAL);
 
         // dessiner la trace
         int size = etat.getSizeHistory();
-        int LINEX = Etat.X_OVAL + Etat.LARG_OVAL / 2;
+        int LINEX = Etat.X_OVAL + LARG_OVAL / 2;
 
         g.setColor(Color.BLUE);
         for (int i = 1; i < size; i++) {
-            g.drawLine(LINEX + (i - 1) * 10, etat.getHistory(size - i), LINEX + i * 10, etat.getHistory(size - 1 - i));
+            g.drawLine(LINEX + (i - 1) * 10, HAUTEUR_FENETRE - etat.getHistory(size - i) - HAUT_OVAL / 2,
+                    LINEX + i * 10, HAUTEUR_FENETRE - etat.getHistory(size - 1 - i) - HAUT_OVAL / 2);
         }
     }
 
